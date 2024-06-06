173f1bb917deec491ff3637afdb616acc77e13b2
monzuralam <monzur707@gmail.com>
2024-06-06 07:31:21 +0600

    shortcode column has been added on shortcode-builder

--- includes/Hooks.php
+++ includes/Hooks.php
@@ -23,7 +23,43 @@ class Hooks {
      * Constructor
      */
     public function __construct() {
+        add_filter('manage_shortcode-builder_posts_columns', array($this, 'add_columns'), 100);
+        add_action('manage_shortcode-builder_posts_custom_column', array($this, 'column_content'), 10, 2);
+    }
 
+    /**
+     * Add Column
+     *
+     * @param array $columns
+     * @return void
+     */
+    public function add_columns($columns) {
+        unset( $columns['date'] );
+        $columns['shortcode']   = __('Shortcode', 'uts');
+
+        return $columns;
+    }
+
+    /**
+     * Column Content
+     *
+     * @param array $column
+     * @param integer $post_id
+     * @return void
+     */
+    public function column_content($column, $post_id) {
+        switch ($column) {
+            case 'shortcode':
+                ob_start();
+?>
+                <input type="text" value="<?php echo esc_attr('[uts id="' . $post_id . '"]') ?>">
+                <button type="button" class="button uts-copy-shortcode" title="<?php echo esc_attr__('Copy', 'uts'); ?>">
+                    <span class="dashicons dashicons-admin-page"></span>
+                </button>
+<?php
+                echo ob_get_clean();
+                break;
+        }
     }
 
     /**
@@ -31,8 +67,8 @@ class Hooks {
      *
      * @return void
      */
-    public static function instance(){
-        if( null == self::$instance ){
+    public static function instance() {
+        if (null == self::$instance) {
             self::$instance = new self();
         }
 
